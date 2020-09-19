ab0c6974d107934e640e4a3183580517488d7a48
camo <camo@camo-PC>
2020-09-19 08:53:01 -0500

    Ingreso de botones y reportes

--- src/A_Vista/MenuView.java
+++ src/A_Vista/MenuView.java
@@ -31,8 +31,22 @@ public class MenuView extends javax.swing.JPanel {
         jLabel4 = new javax.swing.JLabel();
         btnReportes = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
-        jPanel7 = new javax.swing.JPanel();
-        jPanel6 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        jButton3 = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
+        jButton4 = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        jButton5 = new javax.swing.JButton();
+        jLabel9 = new javax.swing.JLabel();
+        jButton6 = new javax.swing.JButton();
+        jLabel10 = new javax.swing.JLabel();
+        jButton7 = new javax.swing.JButton();
+        jLabel11 = new javax.swing.JLabel();
+        jButton8 = new javax.swing.JButton();
+        jLabel12 = new javax.swing.JLabel();
 
         setLayout(new java.awt.GridLayout(1, 2));
 
@@ -44,17 +58,17 @@ public class MenuView extends javax.swing.JPanel {
 
         add(jPanel1);
 
-        jPanel3.setLayout(new java.awt.GridLayout(5, 3));
+        jPanel3.setLayout(new java.awt.GridLayout(13, 2));
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 240, Short.MAX_VALUE)
+            .addGap(0, 285, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 101, Short.MAX_VALUE)
+            .addGap(0, 39, Short.MAX_VALUE)
         );
 
         jPanel3.add(jPanel2);
@@ -63,11 +77,11 @@ public class MenuView extends javax.swing.JPanel {
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 240, Short.MAX_VALUE)
+            .addGap(0, 285, Short.MAX_VALUE)
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 101, Short.MAX_VALUE)
+            .addGap(0, 39, Short.MAX_VALUE)
         );
 
         jPanel3.add(jPanel4);
@@ -90,31 +104,53 @@ public class MenuView extends javax.swing.JPanel {
         jLabel2.setText("Generación de reportes");
         jPanel3.add(jLabel2);
 
-        javax.swing.GroupLayout jPanel7Layout = new javax.swing.GroupLayout(jPanel7);
-        jPanel7.setLayout(jPanel7Layout);
-        jPanel7Layout.setHorizontalGroup(
-            jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 240, Short.MAX_VALUE)
-        );
-        jPanel7Layout.setVerticalGroup(
-            jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 101, Short.MAX_VALUE)
-        );
+        jButton1.setText("Área de Dependencia");
+        jPanel3.add(jButton1);
 
-        jPanel3.add(jPanel7);
+        jLabel5.setText("Administración de Área de Dependencia");
+        jPanel3.add(jLabel5);
 
-        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
-        jPanel6.setLayout(jPanel6Layout);
-        jPanel6Layout.setHorizontalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 240, Short.MAX_VALUE)
-        );
-        jPanel6Layout.setVerticalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 101, Short.MAX_VALUE)
-        );
+        jButton2.setText("Color");
+        jPanel3.add(jButton2);
 
-        jPanel3.add(jPanel6);
+        jLabel6.setText("Administración de Color");
+        jPanel3.add(jLabel6);
+
+        jButton3.setText("Cuenta Contable");
+        jPanel3.add(jButton3);
+
+        jLabel7.setText("Administración de Cuenta Contable");
+        jPanel3.add(jLabel7);
+
+        jButton4.setText("Estado");
+        jPanel3.add(jButton4);
+
+        jLabel8.setText("Administración de Estado");
+        jPanel3.add(jLabel8);
+
+        jButton5.setText("Marca");
+        jPanel3.add(jButton5);
+
+        jLabel9.setText("Administración de Marca");
+        jPanel3.add(jLabel9);
+
+        jButton6.setText("Tipo de Mueble");
+        jPanel3.add(jButton6);
+
+        jLabel10.setText("Administración de Tipo de Mueble");
+        jPanel3.add(jLabel10);
+
+        jButton7.setText("Ubicación");
+        jPanel3.add(jButton7);
+
+        jLabel11.setText("Administración de Ubicación");
+        jPanel3.add(jLabel11);
+
+        jButton8.setText("Usuario");
+        jPanel3.add(jButton8);
+
+        jLabel12.setText("Administración de Usuario");
+        jPanel3.add(jLabel12);
 
         add(jPanel3);
     }// </editor-fold>//GEN-END:initComponents
@@ -124,15 +160,29 @@ public class MenuView extends javax.swing.JPanel {
     public javax.swing.JButton btnBienes;
     public javax.swing.JButton btnInventario;
     public javax.swing.JButton btnReportes;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
-    private javax.swing.JPanel jPanel6;
-    private javax.swing.JPanel jPanel7;
     // End of variables declaration//GEN-END:variables
 }
